caabc2d7af886533af07e44a2c9f860323a17ee1
Shredder <shredder-rull@yandex.ru>
2015-01-26 00:06:03 +0600

    Minifix for Arel patch

--- lib/hstore_accessor/arel_patch.rb
+++ lib/hstore_accessor/arel_patch.rb
@@ -1,15 +1,19 @@
 module HstoreAccessor
   module ArelPath
 
-    def visit_Arel_Attributes_Attribute o, collector
+    def self.included(base)
+      base.alias_method_chain :visit_Arel_Attributes_Attribute, :hstore_support
+    end
+
+    def visit_Arel_Attributes_Attribute_with_hstore_support o, collector
 
       join_name = o.relation.table_alias || o.relation.name
 
       engine = if o.relation.engine == ActiveRecord::Base
-          ActiveRecord::Base.subclasses.index_by(&:table_name)[o.relation.name]
-        else
-          o.relation.engine
-        end
+                 ActiveRecord::Base.subclasses.index_by(&:table_name)[o.relation.name]
+               else
+                 o.relation.engine
+               end
 
       if engine and engine.is_hstore_attribute?(o.name)
         hstore_attribute = engine.hstore_parent_attribute(o.name)
@@ -18,10 +22,9 @@ module HstoreAccessor
         attribute_sql = "(#{attribute_sql})::#{type}" if type = engine.hstore_pg_type(o.name)
         collector << attribute_sql
       else
-        collector << "#{quote_table_name join_name}.#{quote_column_name o.name}"
+        visit_Arel_Attributes_Attribute_without_hstore_support(o, collector)
       end
 
-      collector
     end
 
   end
